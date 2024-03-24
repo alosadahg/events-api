4425e5d0c23aac9d38ecd98153f0baaf12b2d383
hernah <hgalosada@gmail.com>
2024-03-24 22:50:18 +0800

    modified add event and user to event addToApproved

--- src/main/java/com/example/demo/model/event/Event.java
+++ src/main/java/com/example/demo/model/event/Event.java
@@ -1,10 +1,10 @@
 package com.example.demo.model.event;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import jakarta.persistence.*;
 
@@ -24,8 +24,10 @@ public class Event {
     private Integer[] participants;
     private Integer[] reviews;
     private String status;
+
     public Event() {
     }
+
     public Event(String eventname, Integer organizer, String description, String location, LocalDate startdate,
             LocalDate enddate, String status) {
         this.eventname = eventname;
@@ -34,82 +36,119 @@ public class Event {
         this.location = location;
         this.startdate = startdate;
         this.enddate = enddate;
+        setUpvotes(0);
+        setParticipants(new Integer[0]);
+        setReviews(new Integer[0]);
         this.status = status;
     }
+
     public Integer getEid() {
         return eid;
     }
+
     public void setEid(Integer eid) {
         this.eid = eid;
     }
+
     public String getEventname() {
         return eventname;
     }
+
     public void setEventname(String eventname) {
         this.eventname = eventname;
     }
+
     public Integer getOrganizer() {
         return organizer;
     }
+
     public void setOrganizer(Integer organizer) {
         this.organizer = organizer;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getLocation() {
         return location;
     }
+
     public void setLocation(String location) {
         this.location = location;
     }
+
     public LocalDate getStartdate() {
         return startdate;
     }
+
     public void setStartdate(LocalDate startdate) {
         this.startdate = startdate;
     }
+
     public LocalDate getEnddate() {
         return enddate;
     }
+
     public void setEnddate(LocalDate enddate) {
         this.enddate = enddate;
     }
+
     public Integer getUpvotes() {
         return upvotes;
     }
+
     public void setUpvotes(Integer upvotes) {
         this.upvotes = upvotes;
     }
+
     public Integer[] getParticipants() {
         return participants;
     }
+
     public void setParticipants(Integer[] participants) {
         this.participants = participants;
     }
+
     public Integer[] getReviews() {
         return reviews;
     }
+
     public void setReviews(Integer[] reviews) {
         this.reviews = reviews;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
+
     @Override
     public String toString() {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         try {
+            if(upvotes == null) {
+                upvotes = 0;
+            }
+            if (participants == null) {
+                participants = new Integer[0];
+            }
+            if (reviews == null) {
+                reviews = new Integer[0];
+            }
             return objectMapper.writeValueAsString(this);
         } catch (JsonProcessingException e) {
-            e.printStackTrace(); 
+            e.printStackTrace();
             return "transaction failed";
         }
     }
+
 }
--- src/main/java/com/example/demo/service/AttendEventService.java
+++ src/main/java/com/example/demo/service/AttendEventService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.model.attendevent.AttendEvent;
 import com.example.demo.model.attendevent.AttendEventRepository;
+import com.example.demo.model.event.Event;
+import com.example.demo.model.event.EventRepository;
 
 @Service
 public class AttendEventService {
@@ -14,28 +17,41 @@ public class AttendEventService {
     @Autowired
     private AttendEventRepository attendeventrepo;
 
+    @Autowired
+    private EventRepository eventRepo;
+
     public List<AttendEvent> getAll() {
         return attendeventrepo.findAll();
     }
 
     public String addToPending(Integer userid, Integer eventid) {
-        if(attendeventrepo.findByEventidAndUserid(eventid, userid).isEmpty()) {
+        if (attendeventrepo.findByEventidAndUserid(eventid, userid).isEmpty()) {
             AttendEvent newRecord = new AttendEvent(eventid, userid, "interested");
             attendeventrepo.save(newRecord);
             return newRecord.toString();
         }
-        return "Transaction failed. User has existing record.";
+        return "Transaction failed. Already has existing record.";
     }
 
     public String addToApproved(Integer userid, Integer eventid) {
-        if(!attendeventrepo.findByEventidAndUserid(eventid, userid).isEmpty()) {
+        if (!attendeventrepo.findByEventidAndUserid(eventid, userid).isEmpty()) {
             AttendEvent record = attendeventrepo.findByEventidAndUserid(eventid, userid).get(0);
             record.setStatus("approved");
             attendeventrepo.save(record);
+            if (eventid != null) {
+                Event e = eventRepo.findById(eventid).get();
+                if(e.getParticipants()==null) {
+                    e.setParticipants(new Integer[0]);
+                }
+                Integer[] participants = e.getParticipants();
+                Integer[] updatedParticipants = Arrays.copyOf(participants, participants.length + 1);
+                updatedParticipants[participants.length] = userid;
+                e.setParticipants(updatedParticipants);
+                eventRepo.save(e);
+            }
             return record.toString();
         }
-        return "Transaction failed. User has no record.";
+        return "Transaction failed. No record found.";
     }
 
-    
 }
